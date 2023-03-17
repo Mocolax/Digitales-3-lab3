fd738f9ad8b26929e050e7581851b4d4fb798afe
Mocolax <127907365+Mocolax@users.noreply.github.com>
2023-03-17 07:44:50 -0500

    Add files via upload

---
+++ Installer.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+if [ $(ls -d Digitales-3-lab3) ]
+then
+	echo "Archivos ya descargados y compilados"
+	cd Digitales-3-lab3
+else
+	git clone https://github.com/Mocolax/Digitales-3-lab3
+	chmod 777 Digitales-3-lab3
+	cd Digitales-3-lab3
+	gcc EjercicioA.c -o ejercicioA
+	gcc EjercicioB.c -o ejercicioB
+	gcc EjercicioC.c -o ejercicioC
+	gcc EjercicioD.c -o ejercicioD
+fi
+
+(time ./ejercicioB) 2> ../Nose.txt
+grep 'real' ../Nose.txt | tr -d '[a-z]' | sed -e 's/^	0//' > ../datei.txt
+c=$(cat ../datei.txt)
+
+(time python3 ./Lab32.py) 2> ../Nose.txt
+grep 'real' ../Nose.txt | tr -d '[a-z]' | sed -e 's/^	0//' > ../datei.txt
+py=$(cat ../datei.txt)
+echo "$c $py"
+decimal=$(printf "%d" $c)
+decimal2=$(printf "%d" $py)
+echo $decimal $decimal2
+echo "$(($decimal2 - $decimal)), ">> csv.csv
+#al hallar las admitancias de los fundamentales
+#el error entre las impedancias fundamentales y las totales
